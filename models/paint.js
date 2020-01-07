1af79180b7c81d755cc045c6e44a2288bdf447f0
Olivia <olivia.wright101@gmail.com>
2020-01-07 16:40:06 +0000

    Completed paint and paint tests

--- models/paint.js
+++ models/paint.js
@@ -1,3 +1,17 @@
 const Paint = function(litres){
   this.litres = litres
 };
+
+Paint.prototype.checkIfEmpty = function () {
+  if (this.litres === 0){
+    return true
+  } else {
+    return false
+  }
+};
+
+Paint.prototype.emptyPaintCan = function () {
+  this.litres = 0
+};
+
+module.exports = Paint;
--- models/specs/paint_spec.js
+++ models/specs/paint_spec.js
@@ -1,2 +1,32 @@
 const assert = require('assert')
 const Paint = require('../paint.js')
+
+describe('Paint', function(){
+
+  let paint1;
+
+  beforeEach(function (){
+    paint1 = new Paint(2);
+    paint2 = new Paint(0);
+  })
+
+  it('has paint', function(){
+      const expected = false;
+      const actual = paint1.checkIfEmpty();
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should check if it is empty', function(){
+    const expected = true;
+    const actual = paint2.checkIfEmpty();
+    assert.strictEqual(actual, expected);
+  })
+
+  it('should be able to empty', function(){
+    paint1.emptyPaintCan()
+    const expected = 0
+    const actual = paint1.litres;
+    assert.strictEqual(actual, expected)
+  })
+
+});
