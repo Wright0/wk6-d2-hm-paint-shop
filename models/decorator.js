6068062743d89e674011e68bf6ad9e3b9c3d2fe4
Olivia <olivia.wright101@gmail.com>
2020-01-07 18:03:48 +0000

    Spent an hour trying to get something to work only to realize I'd forgotten to set the actual to the thing I was testing.

--- models/decorator.js
+++ models/decorator.js
@@ -2,8 +2,18 @@ const Decorator = function(){
   this.paintStock = [];
 };
 
-Decorator.prototype.addPaintCan = function(){
-  this.paintStock += 1;
+Decorator.prototype.addPaintCan = function(paintCan){
+  this.paintStock.push(paintCan);
 };
+
+Decorator.prototype.checkStockLitres = function(){
+  let litres = 0
+
+  for (let i = 0; i < this.paintStock.length; i++) {
+    litres += this.paintStock[i].litres
+  }
+  return litres;
+};
+
 
 module.exports = Decorator;
--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -11,7 +11,7 @@ describe('Decorator', function(){
 
   beforeEach(function(){
     decorator = new Decorator()
-    paintCan1 = new PaintCan(2)
+    paintCan1 = new PaintCan(5)
     paintCan2 = new PaintCan(3)
   })
 
@@ -20,16 +20,18 @@ describe('Decorator', function(){
     assert.strictEqual(actual, 0);
   })
 
-  // it('should be able to add a paint can to stock', function(){
-  //   decorator.addPaintCan();
-  //   actual = decorator.paintStock;
-  //   assert.strictEqual(actual, 1);
-  // })
-  //
-  // it('should be able to calculate litres in stock', function(){
-  //   decorator.checkStockLitres();
-  //   assert.strictEqual(actual, 5);
-  // })
+  it('should be able to add a paint can to stock', function(){
+    decorator.addPaintCan(paintCan1);
+    actual = decorator.paintStock.length;
+    assert.strictEqual(actual, 1);
+  })
+
+  it('should be able to calculate litres in stock', function(){
+    decorator.addPaintCan(paintCan1);
+    decorator.addPaintCan(paintCan2);
+    actual = decorator.checkStockLitres();
+    assert.strictEqual(actual, 8);
+  })
 
   it('should be able to calculate if they have enough paint for a room')
 
